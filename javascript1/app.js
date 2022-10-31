5912973e313a7762aa6b3260647b1c6495f84db8
pavan63029 <pavanirrinki01@gmail.com>
2022-10-31 22:02:54 +0530

    added javascript drumskit and digital clock assignment

---
+++ javascript1/app.js
@@ -0,0 +1,44 @@
+let a=document.getElementById("w")
+let b=document.getElementById("k")
+let c=document.getElementById("l")
+let d=document.getElementById("m")
+let e=document.getElementById("n")
+
+function play(event){
+    a.innerHTML=`<audio><source src=""  type='mp3'
+    </audio>`
+}
+function play1(){
+    b.innerHTML=`<audio><source src=""  type='mp3'
+    </audio>`
+}
+function play2(){
+    c.innerHTML=`<audio><source src=""  type='mp3'
+    </audio>`
+}
+function play3(){
+    d.innerHTML=`<audio><source src=""  type='mp3'
+    </audio>`
+}
+function play4(){
+    e.innerHTML=`<audio><source src=""  type='mp3'
+    </audio>`
+}
+function play5(){
+    a.textContent="A"
+}
+
+function play9(){
+    e.textContent="E"
+}
+function play8(){
+    d.textContent="D"
+}
+function play7(){
+    c.textContent="C"
+}
+function play6(){
+    b.textContent="B"
+}
+
+
